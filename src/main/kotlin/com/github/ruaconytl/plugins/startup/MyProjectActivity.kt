7fff60a5e8ce6d4bdd162e93f2a082b6aad2074e
GitHub Action <action@github.com>
2025-08-28 10:17:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/ruaconytl/plugins/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/ruaconytl/plugins/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.ruaconytl.plugins.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
